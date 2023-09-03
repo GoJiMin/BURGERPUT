ca053b6f21db2ccb34b0e66f7d06ade5c2798d19
GoJiMin <gojimin3095@gmail.com>
2023-09-03 20:01:16 +0900

    feat: 이메일, 아이디 입력 폼 기능 구현
    - 이메일,아이디 유효성 검사 기능 구현

--- src/main/frontend/src/pages/Admins.jsx
+++ src/main/frontend/src/pages/Admins.jsx
@@ -1,11 +1,55 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 import styles from "./Admins.module.css";
-import { Link } from "react-router-dom";
 
 export default function Admins() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting, isSubmitted, errors },
+  } = useForm();
+
   return (
-    <section className={styles.container}>
-      <div>test</div>
+    <section>
+      <form
+        onSubmit={handleSubmit(async (data) => {
+          await new Promise((r) => setTimeout(r, 1000));
+          alert(JSON.stringify(data));
+        })}
+      >
+        <label htmlFor='email'>이메일</label>
+        <input
+          id='email'
+          type='email'
+          placeholder='test@email.com'
+          aria-invalid={isSubmitted ? (errors.email ? true : false) : undefined}
+          {...register("email", {
+            required: "이메일은 필수 입력 사항입니다.",
+            pattern: {
+              value: /\S+@\S+\.\S+/,
+              message: "이메일 형식에 맞지 않습니다.",
+            },
+          })}
+        />
+        {errors.email && <small role='alert'>{errors.email.message}</small>}
+        <label htmlFor='id'>아이디</label>
+        <input
+          id='id'
+          type='id'
+          placeholder='your ID'
+          {...register("id", {
+            required: "아이디는 필수 입력 사항입니다.",
+            pattern: {
+              value: /^[a-zA-z0-9]{4,12}$/,
+              message: "아이디 형식에 맞지 않습니다.",
+            },
+          })}
+        />
+        {errors.id && <small role='alert'>{errors.id.message}</small>}
+        <button type='submit' disabled={isSubmitting}>
+          제출
+        </button>
+      </form>
     </section>
   );
 }
