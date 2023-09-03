613f482ab87e781202fe33962ac55a7e051e71db
GoJiMin <gojimin3095@gmail.com>
2023-09-03 18:26:39 +0900

    feat: 관리자 계정 추가 페이지 구현

--- src/main/frontend/src/pages/Admins.jsx
+++ src/main/frontend/src/pages/Admins.jsx
@@ -5,30 +5,7 @@ import { Link } from "react-router-dom";
 export default function Admins() {
   return (
     <section className={styles.container}>
-      <Link className={styles.wrapper} to='/select/machines'>
-        {/* <img
-          className={styles.pixel}
-          src='/img/pixelmachine.png'
-          alt='pixelmachine'
-        /> */}
-        <div className={styles.button}>기기 선택</div>
-      </Link>
-      <Link className={styles.wrapper} to='/select/foods'>
-        {/* <img
-          className={styles.pixel}
-          src='/img/pixelfood.png'
-          alt='pixelmachine'
-        /> */}
-        <div className={styles.button}>식품 선택</div>
-      </Link>
-      <Link className={styles.wrapper} to='/select/managers'>
-        {/* <img
-          className={styles.pixel}
-          src='/img/pixelmanager.png'
-          alt='pixelmachine'
-        /> */}
-        <div className={styles.button}>관리자 목록 수정</div>
-      </Link>
+      <div>test</div>
     </section>
   );
 }
