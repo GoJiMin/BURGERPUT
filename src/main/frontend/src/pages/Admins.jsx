8f759da59258726e83580a21f7349a699bf4f93d
GoJiMin <gojimin3095@gmail.com>
2023-09-06 10:03:18 +0900

    design: 관리자 계정 추가 페이지 UI 구현

--- src/main/frontend/src/pages/Admins.jsx
+++ src/main/frontend/src/pages/Admins.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { useForm } from "react-hook-form";
 import styles from "./Admins.module.css";
+import { useForm } from "react-hook-form";
+import { useNavigate, useOutletContext } from "react-router-dom";
 
 export default function Admins() {
   const {
@@ -8,48 +9,123 @@ export default function Admins() {
     handleSubmit,
     formState: { isSubmitting, isSubmitted, errors },
   } = useForm();
+  const { handleHidden } = useOutletContext();
+  const navigate = useNavigate();
+  const handleClick = () => {
+    handleHidden();
+    navigate("/");
+  };
 
   return (
-    <section>
+    <section className={styles.section}>
+      <div className={styles.header}>
+        <div className={styles.title}>관리자 정보 입력</div>
+      </div>
       <form
+        id='inputForm'
+        className={styles.form}
         onSubmit={handleSubmit(async (data) => {
           await new Promise((r) => setTimeout(r, 1000));
           alert(JSON.stringify(data));
         })}
       >
-        <label htmlFor='email'>이메일</label>
-        <input
-          id='email'
-          type='email'
-          placeholder='test@email.com'
-          aria-invalid={isSubmitted ? (errors.email ? true : false) : undefined}
-          {...register("email", {
-            required: "이메일은 필수 입력 사항입니다.",
-            pattern: {
-              value: /\S+@\S+\.\S+/,
-              message: "이메일 형식에 맞지 않습니다.",
-            },
-          })}
-        />
-        {errors.email && <small role='alert'>{errors.email.message}</small>}
-        <label htmlFor='id'>아이디</label>
-        <input
-          id='id'
-          type='id'
-          placeholder='your ID'
-          {...register("id", {
-            required: "아이디는 필수 입력 사항입니다.",
-            pattern: {
-              value: /^[a-zA-z0-9]{4,12}$/,
-              message: "아이디 형식에 맞지 않습니다.",
-            },
-          })}
-        />
-        {errors.id && <small role='alert'>{errors.id.message}</small>}
-        <button type='submit' disabled={isSubmitting}>
-          제출
-        </button>
+        <div className={styles.inputBox}>
+          <label className={styles.label} htmlFor='email'>
+            Email
+          </label>
+          <div className={styles.wrapper}>
+            <input
+              className={styles.input}
+              id='email'
+              type='email'
+              placeholder='example@email.com'
+              autoComplete='off'
+              aria-invalid={
+                isSubmitted ? (errors.email ? true : false) : undefined
+              }
+              {...register("email", {
+                required: "이메일은 필수 입력 사항입니다.",
+                pattern: {
+                  value: /\S+@\S+\.\S+/,
+                  message: "이메일 형식에 맞지 않습니다.",
+                },
+              })}
+            />
+            {errors.email && (
+              <small className={styles.error} role='alert'>
+                {errors.email.message}
+              </small>
+            )}
+          </div>
+        </div>
+
+        <div className={styles.inputBox}>
+          <label className={styles.label} htmlFor='id'>
+            ID
+          </label>
+          <div className={styles.wrapper}>
+            <input
+              className={styles.input}
+              id='id'
+              type='id'
+              placeholder='your ID'
+              autoComplete='off'
+              {...register("id", {
+                required: "아이디는 필수 입력 사항입니다.",
+                pattern: {
+                  value: /^[a-zA-z0-9]{4,12}$/,
+                  message: "아이디 형식에 맞지 않습니다.",
+                },
+              })}
+            />
+            {errors.id && (
+              <small className={styles.error} role='alert'>
+                {errors.id.message}
+              </small>
+            )}
+          </div>
+        </div>
+
+        <div className={styles.inputBox}>
+          <label className={styles.label} htmlFor='password'>
+            Password
+          </label>
+          <div className={styles.wrapper}>
+            <input
+              className={styles.input}
+              id='password'
+              type='password'
+              placeholder='********'
+              autoComplete='off'
+              {...register("password", {
+                required: "비밀번호는 필수 입력 사항입니다.",
+                pattern: {
+                  value: 8,
+                  message: "비밀번호 형식에 맞지 않습니다.",
+                },
+              })}
+            />
+          </div>
+          {errors.password && (
+            <small className={styles.error} role='alert'>
+              {errors.password.message}
+            </small>
+          )}
+        </div>
       </form>
+      <div className={styles.buttons}>
+        <button
+          type='submit'
+          form='inputForm'
+          disabled={isSubmitting}
+          className={styles.button1}
+        >
+          저장
+        </button>
+        <button className={styles.button} onClick={handleClick}>
+          취소
+        </button>
+      </div>
     </section>
   );
 }
