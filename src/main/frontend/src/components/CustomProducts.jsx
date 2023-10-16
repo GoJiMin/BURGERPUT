14975bb170ea808ad051b686cf1bdaa49a3d8d9d
GoJiMin <gojimin3095@gmail.com>
2023-10-16 16:11:40 +0900

    refactor: CustomProducts.jsx UI, 비즈니스 로직 분리

--- src/main/frontend/src/components/CustomProducts.jsx
+++ src/main/frontend/src/components/CustomProducts.jsx
@@ -1,25 +1,12 @@
 import React, { useState } from "react";
 import styles from "./CustomProducts.module.css";
+import { customProductsHook } from "../hooks/useCustomProducts";
 
 export default function CustomProducts({
   value: { name, min, max, id },
   setProducts,
 }) {
-  const [checked, setChecked] = useState(false);
-  const handleChange = (e) => {
-    setChecked((prev) => !prev);
-    handleCheck(e.target.checked);
-  };
-  const deleteProducts = (id) => {
-    setProducts((prev) => prev.filter((product) => product.id !== id));
-  };
-  const handleCheck = (isChecked) => {
-    if (isChecked) {
-      setProducts((prev) => [...prev, { id, isChecked }]);
-    } else {
-      deleteProducts(id);
-    }
-  };
+  const { checked, handleChange } = customProductsHook();
 
   return (
     <div className={styles.product}>
