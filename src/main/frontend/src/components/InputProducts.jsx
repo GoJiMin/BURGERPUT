e8ad50c07fe4a24821601672d721092a7d02a505
GoJiMin <gojimin3095@gmail.com>
2023-10-16 16:05:52 +0900

    refactor: InputProducts.jsx UI, 비즈니스 로직 분리

--- src/main/frontend/src/components/InputProducts.jsx
+++ src/main/frontend/src/components/InputProducts.jsx
@@ -1,43 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import styles from "./InputProducts.module.css";
 import Banner from "./Banner";
+import { useInputProducts } from "../hooks/useInputProducts";
 
 export default function InputProducts({
   product: { name, min, max },
   product,
 }) {
-  const [temp, setTemp] = useState("");
-  const [warning, setWarning] = useState(false);
-  const [missing, setMissing] = useState(false);
-  const handleChange = (e) => {
-    setTemp(e.target.value);
-    setTimeout(() => {
-      if (
-        parseInt(e.target.value) > parseInt(max) ||
-        parseInt(e.target.value) < parseInt(min)
-      ) {
-        setWarning(true);
-        setTimeout(() => {
-          setWarning(false);
-          setTemp("");
-        }, 2300);
-      }
-    }, 2000);
-  };
-
-  const handleClick = () => {
-    if (missing) {
-      setTemp("");
-      setMissing((prev) => !prev);
-      return;
-    }
-    setTemp("999");
-    setMissing((prev) => !prev);
-  };
+  const { temp, warning, handleChange, handleClick } = useInputProducts();
 
   useEffect(() => {
     product.temp = temp;
   }, [product, temp]);
+
   return (
     <div className={styles.product}>
       <div className={styles.text}>
---
+++ src/main/frontend/src/hooks/useInputProducts.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+export function useInputProducts() {
+  const [temp, setTemp] = useState("");
+  const [warning, setWarning] = useState(false);
+  const [missing, setMissing] = useState(false);
+
+  const handleChange = (e) => {
+    setTemp(e.target.value);
+    setTimeout(() => {
+      if (
+        parseInt(e.target.value) > parseInt(max) ||
+        parseInt(e.target.value) < parseInt(min)
+      ) {
+        setWarning(true);
+        setTimeout(() => {
+          setWarning(false);
+          setTemp("");
+        }, 2300);
+      }
+    }, 2000);
+  };
+
+  const handleClick = () => {
+    if (missing) {
+      setTemp("");
+      setMissing((prev) => !prev);
+      return;
+    }
+    setTemp("999");
+    setMissing((prev) => !prev);
+  };
+
+  return { temp, warning, handleChange, handleClick };
+}
